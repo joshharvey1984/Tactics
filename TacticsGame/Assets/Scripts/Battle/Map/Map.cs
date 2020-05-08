005ddcc1ea30fe0cb73ba1e76c8006d9ec34dde0
Josh <joshharvey@btopenworld.com>
2020-05-08 11:38:02 +0100

    Basic Map Setup

---
+++ TacticsGame/Assets/Scripts/Battle/Map/Map.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace TacticsGame.Battle.Map
+{
+    public class Map : MonoBehaviour
+    {
+        private const int MapSizeX = 10, MapSizeZ = 10;
+        
+        private void Awake()
+        {
+            CreateMapTiles();
+        }
+
+        private static void CreateMapTiles()
+        {
+            for (var x = 0; x < MapSizeX; x++) {
+                for (var z = 0; z < MapSizeZ; z++) {
+                    var mapTile = new MapTile(x, z);
+                }
+            }
+        }
+        
+    }
+}
---
+++ TacticsGame/Assets/Scripts/Battle/Map/MapTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TacticsGame.Battle.Map
+{
+    public class MapTile
+    {
+
+        public static List<MapTile> All;
+        
+        private int _mapPosX, _mapPosZ;
+
+        public MapTile(int mapPosX, int mapPosZ)
+        {
+            _mapPosX = mapPosX;
+            _mapPosZ = mapPosZ;
+        }
+    }
+}
